a686c793a832de583cf940a93cefb62cf5373112
aaron <aaronzzxup@gmail.com>
2021-12-09 18:39:12 +0800

    增加代码

--- src/com/aaron/binarytree/refactor/Test.kt
+++ src/com/aaron/binarytree/refactor/Test.kt
@@ -1,26 +1,33 @@
 package com.aaron.binarytree.refactor
 
-import com.aaron.binarytree.ktx.println
-
 /**
  * @author aaronzzxup@gmail.com
  * @since 2021/12/3
  */
-fun main() {
-    val tree = getTree()
-    println(
-        """
-        size: ${tree.size}
-        height: ${tree.height()}
-        toList: ${tree.toList()}
-    """.trimIndent())
-    BinaryPrintTree(tree as BinaryTree<*>).println()
-    println("$tree")
-}
+//fun main() {
+//    val tree = getTree()
+//    println(
+//        """
+//        size: ${tree.size}
+//        height: ${tree.height()}
+//        toList: ${tree.toList()}
+//    """.trimIndent())
+//    BinaryPrintTree(tree as BinaryTree<*>).println()
+//    println("$tree")
+//}
+//
+//private fun getTree(): Tree<Int> {
+//    val tree = AVLTree<Int>()
+//    val array = (1..31).toList()
+//    tree.addAll(array)
+//    return tree
+//}
 
-private fun getTree(): Tree<Int> {
-    val tree = AVLTree<Int>()
-    val array = (1..31).toList()
-    tree.addAll(array)
-    return tree
+fun main() {
+    val list = (1..10).toList()
+    val tree = BTree<Int>(4)
+    list.forEach {
+        tree.add(it)
+    }
+    println("${tree.root}")
 }
